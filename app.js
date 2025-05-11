57959689ab18f800f351bde5bfed5442b16ba85e
AbdulHaseeb-4 <abdulhaseebshahid4@gmail.com>
2025-05-11 11:59:02 -0700

    While

---
+++ app.js
@@ -0,0 +1,20 @@
+let i = 0;
+while (i < 10){
+    ++i
+    if (i === 5){
+        continue
+    }
+    console.log(i)
+}
+let u = 0;
+let MyName = "Abdul Haseeb"
+console.log(MyName);
+
+while (u < 10){
+    
+    if (u === 8){
+        break
+    }
+    console.log(u)
+    ++u
+}
