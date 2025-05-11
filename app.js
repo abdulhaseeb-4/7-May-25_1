c697a0653c0c5c4369a1039b99e5c302fdd585f3
AbdulHaseeb-4 <abdulhaseebshahid4@gmail.com>
2025-05-11 13:14:22 -0700

    Assignment

--- app.js
+++ app.js
@@ -36,23 +36,52 @@
 
 
 
-let i = 0;
-do {
-    if (i === 5) {
-        break
-    }
-    console.log(i);
-    ++i
-} while (i < 10)
-let MyName = "Abdul Haseeb"
-console.log(MyName);
+// let i = 0;
+// do {
+//     if (i === 5) {
+//         break
+//     }
+//     console.log(i);
+//     ++i
+// } while (i < 10)
+// let MyName = "Abdul Haseeb"
+// console.log(MyName);
 
-let u = 0;
-do {
-    ++u
-    if (u === 8) {
-        continue
-    }
-    console.log(u);
+// let u = 0;
+// do {
+//     ++u
+//     if (u === 8) {
+//         continue
+//     }
+//     console.log(u);
 
-} while (u < 10)
+// } while (u < 10)
+
+
+
+
+
+
+
+
+
+
+    let Find = " Rafale";
+     let Days = 3;
+
+  for (let i = 0; i < 3; i++) {
+   let userInput = prompt("Find the missing plane:");
+
+  if (userInput === Find) {
+    alert("Find the missing plane:" + Find);
+    break;
+  } else {
+    Days--;
+
+    if (Days === 0) {
+      alert("Not found bcz it's Shot Down");
+    } else {
+      alert(`Still missing. You have ${Days} day(s) remain's.`);
+    }
+  }
+}
